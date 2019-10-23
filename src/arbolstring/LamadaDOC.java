460520a3dca257ff0d3e4682c4ad3bf2b54e7253
gordo0195 <44810828+gordo0195@users.noreply.github.com>
2019-10-23 02:54:50 +0200

    sixth commit: the doc and docx extractor has been build and implement

--- src/arbolstring/LamadaDOC.java
+++ src/arbolstring/LamadaDOC.java
@@ -11,6 +11,7 @@ package arbolstring;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.hwpf.usermodel.Paragraph;
@@ -24,7 +25,7 @@ public class LamadaDOC  {
         FileInputStream File;
         WordExtractor extractor;
         HWPFDocument doc;
-        
+        ArbolString Principal = new ArbolString();
         
                 
         try{
@@ -34,12 +35,31 @@ public class LamadaDOC  {
             String texto_extraido = extractor.getText();
             System.out.println(texto_extraido);
             
+            String[] words = texto_extraido.split("\\s+");
+            
+            for (int i = 0; i < words.length; i++) {
+                
+                 words[i] = words[i].replaceAll("[^\\w]", "");
+                 
+            }
+            
+            String temp2 = Arrays.toString(words);
+            
+            
+            int tope = words.length;
+            for(int i = 0; i < tope; i++){
+                
+                Principal.insertar(words[i]);
+                
+            
+            }
+            
             
 
         } catch (IOException e) {
             System.out.println(e);
             }
         
-    
+        Principal.PreOrden(Principal);
     }
 } 
