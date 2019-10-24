49f910e803fc167e032ff68add71cb8a0f9cf207
gordo0195 <44810828+gordo0195@users.noreply.github.com>
2019-10-25 01:09:31 +0200

    tenth commit: implement a file name in a binary tree node

--- src/arbolstring/LamadaPDF.java
+++ src/arbolstring/LamadaPDF.java
@@ -1,38 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arbolstring;
 
 /**
  *
  * @author marco
+ * 
+ * Instituto Tecnológico de  Costa Rica
+ * Area de Ingenieria de Computadores
+ * Algoritmos y estructuras de datos I
+ * Profesor: Antonio Torres.
+ * Alumnos: Marco Picado M.
+ *               Gretchell Ochoa.
+ * Segundo semestre 2019.
  */
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
 /**
  *
- * @author Azeem
+ * implemented by Marco Picado, based in work of Azeem.
  */
 public class LamadaPDF  {
-
+    /**
+     * clase encargada de leer y procesar los caracteres previemente extraidos de un documeento PDF
+     * @param args nulo
+     */
+    
+    
     public static void main(String[] args) {
+        /**
+         * meetodo principal 
+         * lee y devuelve  cada palabra insertada en el árbol
+         */
         
+        String path = "C:\\Users\\Gordo_0195\\Documents\\imagenes progra\\documentacion externa.pdf";
+        File archivo = new File(path);
+        String referencia = archivo.getName();
         ArbolString Principal = new ArbolString();
         LectorPDF pdfManager = new LectorPDF();
-        pdfManager.setFilePath("/home/marco/Documentos/imagenes progra/documentacion externa.pdf");
+        pdfManager.setFilePath(path);
        
-        try{
-            String text = pdfManager.toText();
+        try{            
+            String texto = pdfManager.toText();
+            String text = texto.toLowerCase();
             System.out.println(text);
             String[] words = text.split("\\s+");
             
             for (int i = 0; i < words.length; i++) {
-                 // You may want to check for a non-word character before blindly
-                // performing a replacement
-                // It may also be necessary to adjust the character class
+                
                  words[i] = words[i].replaceAll("[^\\w]", "");
                  
             }
@@ -43,7 +59,7 @@ public class LamadaPDF  {
             int tope = words.length;
             for(int i = 0; i < tope; i++){
                 
-                Principal.insertar(words[i]);
+                Principal.insertar(words[i], referencia);
                 
             
             }
@@ -58,3 +74,4 @@ public class LamadaPDF  {
      }
     
 }
+ 
